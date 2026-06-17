b62b1ba0440b5fdd1078fd5343fdaf68d00e694f
StephenOnyekachi <88212958+StephenOnyekachi@users.noreply.github.com>
2026-06-17 20:21:39 +0100

    Fix render email smtp

--- Project/settings.py
+++ Project/settings.py
@@ -177,13 +177,13 @@ USE_TZ = True
 
 # with env activated
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_TIMEOUT = 30
+EMAIL_TIMEOUT = 60
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
